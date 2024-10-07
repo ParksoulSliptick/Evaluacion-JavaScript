e6613e3b97bc66799a601528e7c55a38fefa017b
ParksoulSliptick <163039921+ParksoulSliptick@users.noreply.github.com>
2024-10-07 13:47:14 -0600

    Listo mi ejercicio

---
+++ script.js
@@ -0,0 +1,27 @@
+var nombre = ("Alejandro");
+var edad = ("20");
+var estudiaste = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(estudiaste);
+
+let saludo = 'Hola soy ${"nombre"} y tengo {"edad"}';
+console.log(saludo)
+
+let si = '{$nombre}si vas a aprobar la materia';
+let no = '{$nombre}no vas a aprobar la materia';
+if (estudiaste > false);
+{
+    alert(no);
+} else {
+    alert (si);
+}
+
+
+
+
+
+
+
+
